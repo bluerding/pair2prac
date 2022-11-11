08f0c029151cfac54f9ecc039ac9516bfc4cdba1
Ben Luerding <bluerding@gmail.com>
2022-11-11 09:52:41 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,9 @@
+var bestFriend = "Ben" 
+var goodFriend = "Maya"
+if (bestFriend === "Ben") {
+    console.log("Hey, best friend!") 
+} else if (goodFriend === "Maya") {
+    console.log("Hi, Maya!")
+} else {
+    console.log ("Hi, stranger")
+}
